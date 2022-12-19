221d712ff4e689fb7653b0c30da84444b83e34b9
Grace Williams <williamsciela@gmail.com>
2022-12-19 00:20:47 -0600

    Auth Controller update

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -10,5 +10,30 @@ use \stdClass;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validator = Validator::make(request->all(), [
+            'firstName'=> 'required|string|max:255',
+            'lastName'=> 'required|string|max:255',
+            'userName'=> 'required|string|max:255|unique:users',
+            'email'=> 'required|string|email|max:255|unique:users',
+            'password'=> 'required|string|min:8'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'firstName'=> $request->firstName,
+            'lastName'=> $request->lastName,
+            'userName'=> $request->userName,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response();
+            json(['data'=>$user, 'access_token' => $token, 'token_type' => 'Bearer',]);
+    }
 }
