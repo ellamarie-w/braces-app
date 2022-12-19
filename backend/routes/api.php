f0a9abe49943f9ca815cc4177b3bd3c052b7ccc8
Grace Williams <williamsciela@gmail.com>
2022-12-19 01:47:36 -0600

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,16 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LibraryItemsController;
 use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -25,11 +15,11 @@ Route::get('/library',[LibraryItemsController::class,'index']);
 
 /*
 |
+| - - - - 
+| Users authentication
+| - - - -
 |
-- - Users authentication
 |
-|
-v
 */
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
