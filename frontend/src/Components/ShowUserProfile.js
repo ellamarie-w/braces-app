1fe27dba92f61de28e06b1583d84afdee7c9f15a
Grace Williams <williamsciela@gmail.com>
2022-12-21 18:52:47 -0600

    Created profile components

---
+++ frontend/src/Components/ShowUserProfile.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import {Link} from react-router-dom
+
+const ShowUserProfile = () => {
+
+    const getAllData = () => {
+        //
+    }
+
+    const deleteData = () => {
+        //
+    }
+
+  return (
+    <div>ShowUserProfile</div>
+  )
+}
+
+export default ShowUserProfile
