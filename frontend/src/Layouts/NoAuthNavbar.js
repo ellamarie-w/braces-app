48f24a120594ac46a28eb1ed5763e589fdc53b72
Grace Williams <williamsciela@gmail.com>
2022-12-22 22:34:22 -0600

    navbar changes

--- frontend/src/Layouts/NoAuthNavbar.js
+++ frontend/src/Layouts/NoAuthNavbar.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import logo from '../Assets/Images/logo.png';
+import logo from '../Assets/Images/logoVariantDarkMode.png';
 
 function NoAuthNavbar(){
     return (
-        <nav className="navbar bg-ligth">
+        <nav className="navbar bg-dark bg-gradient shadow-sm p-1 mb-3 sticky-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                <img src={logo} alt="BracesApp" height="28" />
+                <a className="navbar-brand" href="/">
+                <img src={logo} alt="BracesApp" height="32" />
                 </a>
                 <div className="">
                 <ul className="nav">
                     <li className="nav-item">
-                        <a className="nav-link link-dark" href="#">Login</a>
+                        <a className="nav-link link-light" href="/">Login</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link link-dark" href="#">Sign up</a>
+                        <a className="nav-link link-light" href="/">Sign up</a>
                     </li>
                 </ul>
                 </div>
