18f0307dc578fbd4ca6479a45215029497185e8f
Grace Williams <williamsciela@gmail.com>
2022-12-23 08:55:19 -0600

    Deleted files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/dropdown';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ShowLibrary from './Components/ShowLibrary';
-//import NoAuthLayout from './Layouts/NoAuthLayout';
 import AuthLayout from './Layouts/AuthLayout';
 import Login from './Pages/Login';
 
